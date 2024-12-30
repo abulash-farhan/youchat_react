605cda5885bc2fe1570952aaa6343e7289403d60
Farhan Abulash <farhanabulash1029@gmail.com>
2024-12-30 11:21:15 +0530

    Initial commit

---
+++ backend/index.js
@@ -0,0 +1,105 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoutes=require("./routes/userRoutes");
+const messagesRoute = require("./routes/messagesRoute");
+const socket = require("socket.io");
+const path = require("path");
+
+const app = express();
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+app.use("/api/auth",userRoutes); 
+app.use("/api/messages",messagesRoute); 
+
+// --------------------------deployment------------------------------
+
+const __dirname1 = path.resolve(); // resolving the paths
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build"))); // the react frontend is generally at frontend/build
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+
+// --------------------------deployment------------------------------
+
+
+// db connection
+mongoose.connect(process.env.MONGO_URL
+,{
+}).then(()=>{
+    console.log("DB connected successfully");
+}).catch((err)=>{
+    console.log(err);
+})
+
+
+const server = app.listen(process.env.PORT,()=>{
+    console.log(`Server Started on Port ${process.env.PORT}`)
+});
+
+const io = socket(server,{
+    cors:{
+        origin:"https://youchatapp.onrender.com",
+        methods: ["POST","GET"],
+        credentials:true
+    },
+})
+
+global.onlineUsers = new Map();
+io.on("connection",(socket)=>{
+    global.chatSocket=socket;
+    socket.on("add-user",(userId)=>{
+        global.onlineUsers.set(userId,socket.id);
+    });
+    let temp={};
+    for(let key of onlineUsers.keys()){
+        temp[key]=true;
+    }
+    io.sockets.emit("onlineUsers",temp);
+   
+    socket.on("send-msg",(data)=>{
+        const sendUserSocket=onlineUsers.get(data.to);
+        if(sendUserSocket!=undefined){
+            const messageData = {
+                recipientSocketId: sendUserSocket,
+                message: data.message
+            };
+            io.sockets.emit("msg-received", messageData);
+        }
+    });
+
+    socket.on("logout",(data)=>{
+        const val = data._id;
+        onlineUsers.delete(val);
+        let temp={};
+        for(let key of onlineUsers.keys()){
+            temp[key]=true;
+        }
+        io.sockets.emit("onlineUsers",temp);
+    });
+
+    socket.on("disconnect", () => { 
+        global.onlineUsers.forEach((value, key) => {
+            if (value === socket.id) {
+                global.onlineUsers.delete(key);
+                return;
+            }
+        });
+        let temp={};
+        for(let key of onlineUsers.keys()){
+            temp[key]=true;
+        }
+        io.sockets.emit("onlineUsers",temp);
+        });
+});
